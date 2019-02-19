0689bdeccd6b9514bc05e3e8c475e49298424989
PGrafkin <pavgra@gmail.com>
2019-02-18 22:58:05 -0500

    Added zuul server Configured auth server and data source manager to register with eureka Configured data source manager to auth via zuul

--- zuul-svr/src/main/java/org/ohdsi/zuulsrv/Application.java
+++ zuul-svr/src/main/java/org/ohdsi/zuulsrv/Application.java
@@ -1,17 +1,13 @@
-package org.ohdsi.authserver;
+package org.ohdsi.zuulsrv;
 
-import com.cosium.spring.data.jpa.entity.graph.repository.support.EntityGraphJpaRepositoryFactoryBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
 @EnableEurekaClient
-@EnableJpaRepositories(
-        repositoryFactoryBeanClass = EntityGraphJpaRepositoryFactoryBean.class,
-        basePackages = {"org.ohdsi.authserver.*"}
-)
 public class Application {
 
     public static void main(String[] args) {
